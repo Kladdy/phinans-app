ba68fd0f0c115a7e99e69f2c610fa91676b436e6
Sigfrid Stjärnholm <sigge.stjarnholm@gmail.com>
2022-04-19 09:04:19 +0200

    Add saving of lable

--- src/components/crypto-wallets/AddWalletFormNiceHash.tsx
+++ src/components/crypto-wallets/AddWalletFormNiceHash.tsx
@@ -24,23 +24,40 @@ export default function AddWalletFormNiceHash({walletFields, setWalletFields}) {
   const { t } = useTranslation('common');
 
   // States
+  const [label, setLabel] = useState("")
   const [organizationId, setOrganizationId] = useState("")
   const [apiKey, setApiKey] = useState("")
   const [apiSecret, setApiSecret] = useState("")
 
   useEffect(() => {
     setWalletFields({
+      label: label,
       organizationId: organizationId,
       apiKey: apiKey,
-      apiSecret: apiSecret
+      apiSecret: apiSecret, 
     })
 
-  }, [organizationId, apiKey, apiSecret])
+  }, [organizationId, apiKey, apiSecret, label])
 
   return (
     <div className="isolate -space-y-px rounded-md shadow-sm">
       
       <div className="relative border border-gray-300 rounded-md rounded-b-none px-3 py-2 focus-within:z-10 focus-within:ring-1 focus-within:ring-indigo-600 focus-within:border-indigo-600">
+        <label htmlFor="label" className="block text-xs font-medium text-gray-900">
+          {t('crypto-wallets.nicehash.label')}
+        </label>
+        <input
+          type="text"
+          name="label"
+          id="label"
+          className="block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm"
+          placeholder={t('crypto-wallets.voluntary')}
+          value={label}
+          onChange={e => setLabel(e.target.value)}
+        />
+      </div>
+      
+      <div className="relative border border-gray-300 rounded-md rounded-b-none rounded-t-none px-3 py-2 focus-within:z-10 focus-within:ring-1 focus-within:ring-indigo-600 focus-within:border-indigo-600">
         <label htmlFor="organization_id" className="block text-xs font-medium text-gray-900">
           {t('crypto-wallets.nicehash.organization_id')}
         </label>
